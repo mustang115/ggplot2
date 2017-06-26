c723ba035440facf7352b84e96798a0608e2860f
mustang115 <supreetkn@gmail.com>
2017-06-26 15:10:52 -0400

    Update ggplothowto.R

--- ggplothowto.R
+++ ggplothowto.R
@@ -1,6 +1,32 @@
 library(ggplot2)
 
+#datasets to practice with
+#mtcars
+#diamonds
+
 # how to make simple plots with ggplot2
+
+# grammer of graphics
+
+# data              # dataset being plotted
+# aesthetics        # Scale to which to map the data
+# geometries        # Visual elements of the data
+# facets
+# statistics
+# co-ordinates
+# themes
+
+# geom layer functionalities
+# geom_point()
+# geom_smooth()     # just adds a smooth line on top of the points
+  # alpha = 0.4     # makes it 40% transparent
+
+
+# aesthetic layer functionalities
+# factor
+# color   # note if you use color and lines, the lines split up based on the color
+# size
+# shape # havent tested yet
 
 # data frame, axis and what type of chart you want
 ggplot(mtcars, aes(x=cyl, y=mpg)) + geom_point()
@@ -8,3 +34,32 @@ ggplot(mtcars, aes(x=cyl, y=mpg)) + geom_point()
 # treat one set of variables as factors ( only that factor level is shown, not treated as number)
 
 ggplot(mtcars, aes(x=factor(cyl), y=mpg)) + geom_point()
+
+# generate color and size of the points based on another variable
+
+ggplot(mtcars, aes(x=wt,y=mpg,color=disp)) + geom_point()
+
+ggplot(mtcars,aes(x=wt,y=mpg, size=disp)) + geom_point()
+
+# shape of the points only makes sense with categorical data
+
+#using geom_smooth
+
+ggplot(diamonds, aes(x=carat, y=price)) + 
+  geom_point() + 
+  geom_smooth()
+  # one thing to note is as soon as you add by color and line, the lines get split by the color
+
+ggplot(diamonds, aes(x = carat, y = price, color = clarity)) +
+  geom_point() +
+  geom_smooth()
+  
+  # alpha option inside the geom_point makes it 40% transparent
+ggplot(diamonds, aes(x = carat, y = price, color = clarity)) +
+  geom_point(alpha=0.4) +
+
+       
+
+
+
+
