ac99c9b436ce380a094d81beab302fac42e1d85d
mustang115 <supreetkn@gmail.com>
2017-07-01 11:27:05 -0400

    Update basegraphics.R

--- basegraphics.R
+++ basegraphics.R
@@ -1,3 +1,10 @@
+# general plot with base graphics
+plot(data_maruti$Close,
+     type = "l",
+     main = "Maruti Closing Values",
+     col = "red", xlab = "Date", ylab = "Index Value")
+
+
 # Plot the correct variables of mtcars
 plot(mtcars$wt, mtcars$mpg, col = mtcars$cyl)
 
