3b5ee6d4f5a28d1ffd929c4cacf51d1017d9738c
mustang115 <supreetkn@gmail.com>
2017-07-14 20:41:42 -0400

    Update basegraphics.R

--- basegraphics.R
+++ basegraphics.R
@@ -1,3 +1,16 @@
+#options I know
+#http://www.statmethods.net/graphs/line.html                #types of plots
+
+#type="l","c"
+#main="Text"
+#xlab="Text"
+#ylab="Text"
+#col=1:4, #col="red", #col=mtcars$cyl, #col=as.factor(mtcars$cyl)
+#labels=1:4
+
+
+
+
 # general plot with base graphics
 plot(data_maruti$Close,
      type = "l",
@@ -13,3 +26,14 @@ mtcars$fcyl <- as.factor(mtcars$cyl)
 
 # Make the same plot as in the first instruction, assign color according to cylinder factor variable
 plot(mtcars$wt,mtcars$mpg, col = mtcars$fcyl)
+
+
+#######################
+
+library(astsa)
+plot(jj, main="J&J Quarterly Earnings per share", type="c")
+text(jj, labels=1:4,col=1:4)
+
+#######################
+
+
